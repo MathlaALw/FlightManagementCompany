6838160c93e7a135d6690ebfb784774b61360f3b
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-12 16:24:35 +0400

    Add FlightDbContext and EF Core package references

---
+++ Data/FlightDbContext.cs
@@ -0,0 +1,38 @@
+﻿using FlightManagementCompany.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlightManagementCompany.Data
+{
+    public class FlightDbContext : DbContext
+    {
+        public DbSet<Aircraft> Aircrafts { get; set; }
+        public DbSet<AircraftMaintenance> AircraftMaintenances { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Baggage> Baggages { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<CrewMember> CrewMembers { get; set; }
+
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<FlightCrew> FlightCrews { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AEOLTE6;Initial Catalog=FlightMSDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
