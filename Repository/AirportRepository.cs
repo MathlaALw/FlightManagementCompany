84af81e0e9956d663852a1fccd6f9871b7dba11d
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 15:51:01 +0400

    Implement Airport repository and interface

---
+++ Repository/AirportRepository.cs
@@ -0,0 +1,45 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class AirportRepository : IAirportRepository
+    {
+        private readonly FlightDbContext _context;
+        public AirportRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Add(Airport airport)
+        {
+            _context.Airports.Add(airport);
+            _context.SaveChanges();
+        }
+
+        public void Update(Airport airport)
+        {
+            _context.Airports.Update(airport);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Airport airport)
+        {
+            _context.Airports.Remove(airport);
+            _context.SaveChanges();
+        }
+
+
+        public Airport GetById(int id) => _context.Airports.Find(id);
+
+        public IEnumerable<Airport> GetAll() => _context.Airports.ToList();
+
+
+    }
+}
---
+++ Repository/IAirportRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IAirportRepository
+    {
+        void Add(Airport airport);
+        void Delete(Airport airport);
+        IEnumerable<Airport> GetAll();
+        Airport GetById(int id);
+        void Update(Airport airport);
+    }
+}
