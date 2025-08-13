73ff3a724660879239216054cbb970f49c677b99
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 16:19:10 +0400

    Add IPassengerRepository interface and implementation

---
+++ Repository/IPassengerRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IPassengerRepository
+    {
+        void Add(Passenger passenger);
+        void Delete(Passenger passenger);
+        IEnumerable<Passenger> GetAll();
+        Passenger GetById(int id);
+        void Update(Passenger passenger);
+    }
+}
---
+++ Repository/PassengerRepository.cs
@@ -0,0 +1,43 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class PassengerRepository : IPassengerRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public PassengerRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Passenger passenger)
+        {
+            _context.Passengers.Add(passenger);
+            _context.SaveChanges();
+        }
+
+        public void Update(Passenger passenger)
+        {
+            _context.Passengers.Update(passenger);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Passenger passenger)
+        {
+            _context.Passengers.Remove(passenger);
+            _context.SaveChanges();
+        }
+
+        public Passenger GetById(int id) => _context.Passengers.Find(id);
+
+        public IEnumerable<Passenger> GetAll() => _context.Passengers.ToList();
+
+    }
+}
