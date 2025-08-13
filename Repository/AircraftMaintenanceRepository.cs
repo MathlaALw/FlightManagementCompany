8f3dcfc70d62e1300dac589055a655a968040f2a
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 14:53:27 +0400

    Add AircraftMaintenanceRepository for database operations

---
+++ Repository/AircraftMaintenanceRepository.cs
@@ -0,0 +1,44 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class AircraftMaintenanceRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public AircraftMaintenanceRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(AircraftMaintenance aircraftMaintenance)
+        {
+            _context.AircraftMaintenances.Add(aircraftMaintenance);
+            _context.SaveChanges();
+        }
+        public void Update(AircraftMaintenance aircraftMaintenance)
+        {
+            _context.AircraftMaintenances.Update(aircraftMaintenance);
+            _context.SaveChanges();
+        }
+
+        public void Delete(AircraftMaintenance aircraftMaintenance)
+        {
+            _context.AircraftMaintenances.Remove(aircraftMaintenance);
+            _context.SaveChanges();
+        }
+
+        public AircraftMaintenance GetById(int id)=>_context.AircraftMaintenances.Find(id);
+        
+
+        public IEnumerable<AircraftMaintenance> GetAll() => _context.AircraftMaintenances.ToList();
+        
+
+
+    }
+}
