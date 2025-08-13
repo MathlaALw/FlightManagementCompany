b984b768cbc6a9e42aca95aa5958bf406a1d5462
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 16:18:18 +0400

    Add FlightCrewRepository and IFlightCrewRepository

---
+++ Repository/FlightCrewRepository.cs
@@ -0,0 +1,45 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class FlightCrewRepository : IFlightCrewRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public FlightCrewRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(FlightCrew flightCrew)
+        {
+            _context.FlightCrews.Add(flightCrew);
+            _context.SaveChanges();
+        }
+
+        public void Update(FlightCrew flightCrew)
+        {
+            _context.FlightCrews.Update(flightCrew);
+            _context.SaveChanges();
+        }
+
+        public void Delete(FlightCrew flightCrew)
+        {
+            _context.FlightCrews.Remove(flightCrew);
+            _context.SaveChanges();
+        }
+
+        public FlightCrew GetById(int id) => _context.FlightCrews.Find(id);
+
+        public IEnumerable<FlightCrew> GetAll() => _context.FlightCrews.ToList();
+
+
+
+    }
+}
---
+++ Repository/IFlightCrewRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IFlightCrewRepository
+    {
+        void Add(FlightCrew flightCrew);
+        void Delete(FlightCrew flightCrew);
+        IEnumerable<FlightCrew> GetAll();
+        FlightCrew GetById(int id);
+        void Update(FlightCrew flightCrew);
+    }
+}
