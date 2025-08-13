e9295982861a652f10ee93d7b11a410ceb8a5970
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 15:57:11 +0400

    Implement Flight repository and interface

---
+++ Repository/FlightRepository.cs
@@ -0,0 +1,58 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class FlightRepository : IFlightRepository
+    {
+        private readonly FlightDbContext _context;
+        public FlightRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Flight flight)
+        {
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+        }
+        public void Update(Flight flight)
+        {
+            _context.Flights.Update(flight);
+            _context.SaveChanges();
+        }
+        public void Delete(Flight flight)
+        {
+            _context.Flights.Remove(flight);
+            _context.SaveChanges();
+        }
+        public Flight GetById(int id) => _context.Flights.Find(id);
+        public IEnumerable<Flight> GetAll() => _context.Flights.ToList();
+
+
+        // Get Flights by Date Range
+        public IEnumerable<Flight> GetFlightsByDateRange(DateTime from, DateTime to)
+        {
+            return _context.Flights
+                .Where(f => f.DepartureUtc >= from && f.ArrivalUtc <= to)
+                .ToList();
+        }
+
+        // Get flight by Route
+        public IEnumerable<Flight> GetFlightsByRoute(int routeId)
+        {
+            return _context.Flights
+                .Where(f => f.RouteId == routeId)
+                .ToList();
+        }
+
+
+
+
+    }
+}
---
+++ Repository/IFlightRepository.cs
@@ -0,0 +1,15 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IFlightRepository
+    {
+        void Add(Flight flight);
+        void Delete(Flight flight);
+        IEnumerable<Flight> GetAll();
+        Flight GetById(int id);
+        IEnumerable<Flight> GetFlightsByDateRange(DateTime from, DateTime to);
+        IEnumerable<Flight> GetFlightsByRoute(int routeId);
+        void Update(Flight flight);
+    }
+}
