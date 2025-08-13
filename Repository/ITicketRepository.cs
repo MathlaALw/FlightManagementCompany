af22edc68a1296ce417eed65cc39c18ef1a3b2aa
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 16:05:05 +0400

    Add ITicketRepository interface and TicketRepository class

---
+++ Repository/ITicketRepository.cs
@@ -0,0 +1,15 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface ITicketRepository
+    {
+        void Add(Ticket ticket);
+        void Delete(Ticket ticket);
+        IEnumerable<Ticket> GetAll();
+        Ticket GetById(int id);
+        IEnumerable<Ticket> GetTicketsByBookingRef(string bookingRef);
+        IEnumerable<Ticket> GetTicketsByPassengerId(int passengerId);
+        void Update(Ticket ticket);
+    }
+}
---
+++ Repository/TicketRepository.cs
@@ -0,0 +1,73 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class TicketRepository : ITicketRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public TicketRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Add(Ticket ticket)
+        {
+            _context.Tickets.Add(ticket);
+            _context.SaveChanges();
+        }
+
+        public void Update(Ticket ticket)
+        {
+            _context.Tickets.Update(ticket);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Ticket ticket)
+        {
+            _context.Tickets.Remove(ticket);
+            _context.SaveChanges();
+        }
+
+        public Ticket GetById(int id) => _context.Tickets.Find(id);
+
+        public IEnumerable<Ticket> GetAll() => _context.Tickets.ToList();
+
+        // Get Tickets by BookingRef
+        public IEnumerable<Ticket> GetTicketsByBookingRef(string bookingRef)
+        {
+
+            // Find the BookingId for the given BookingRef
+            var bookingId = _context.Bookings
+                .Where(b => b.BookingRef == bookingRef)
+                .Select(b => b.BookingId)
+                .FirstOrDefault();
+
+            // Return tickets for that BookingId
+            return _context.Tickets
+                .Where(t => t.BookingId == bookingId)
+                .ToList();
+        }
+
+        // Get Tickets by PassengerId
+        public IEnumerable<Ticket> GetTicketsByPassengerId(int passengerId)
+        {
+            // Find the bookingId for the given PassengerId
+            var bookingId = _context.Bookings
+                .Where(b => b.PassengerId == passengerId)
+                .Select(b => b.BookingId)
+                .FirstOrDefault();
+            // Return tickets for that PassengerId
+            return _context.Tickets
+                .Where(t => t.BookingId == bookingId)
+                .ToList();
+        }
+    }
+}
