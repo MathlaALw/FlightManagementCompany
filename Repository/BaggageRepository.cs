4b9299b8253cf8f30af8e21655ed1985a784f143
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 16:17:23 +0400

    Add Baggage repository and interface implementation

---
+++ Repository/BaggageRepository.cs
@@ -0,0 +1,44 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class BaggageRepository : IBaggageRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public BaggageRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Baggage baggage)
+        {
+            _context.Baggages.Add(baggage);
+            _context.SaveChanges();
+        }
+
+        public void Update(Baggage baggage)
+        {
+            _context.Baggages.Update(baggage);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Baggage baggage)
+        {
+            _context.Baggages.Remove(baggage);
+            _context.SaveChanges();
+        }
+
+        public Baggage GetById(int id) => _context.Baggages.Find(id);
+
+        public IEnumerable<Baggage> GetAll() => _context.Baggages.ToList();
+
+
+    }
+}
---
+++ Repository/IBaggageRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IBaggageRepository
+    {
+        void Add(Baggage baggage);
+        void Delete(Baggage baggage);
+        IEnumerable<Baggage> GetAll();
+        Baggage GetById(int id);
+        void Update(Baggage baggage);
+    }
+}
