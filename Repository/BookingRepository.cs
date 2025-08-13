d112902d32b8ffda324fd05a40a2a770beb50831
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 16:15:16 +0400

    Add Booking repository and interface implementation

---
+++ Repository/BookingRepository.cs
@@ -0,0 +1,52 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class BookingRepository : IBookingRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public BookingRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Booking booking)
+        {
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+        }
+
+        public void Update(Booking booking)
+        {
+            _context.Bookings.Update(booking);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Booking booking)
+        {
+            _context.Bookings.Remove(booking);
+            _context.SaveChanges();
+        }
+
+        public Booking GetById(int id) => _context.Bookings.Find(id);
+
+        public IEnumerable<Booking> GetAll() => _context.Bookings.ToList();
+
+
+        // Get Booking by Date Range 
+        public IEnumerable<Booking> GetBookingsByDateRange(DateTime from, DateTime to)
+        {
+            return _context.Bookings
+                .Where(b => b.BookingDate >= from && b.BookingDate <= to)
+                .ToList();
+        }
+
+    }
+}
---
+++ Repository/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IBookingRepository
+    {
+        void Add(Booking booking);
+        void Delete(Booking booking);
+        IEnumerable<Booking> GetAll();
+        IEnumerable<Booking> GetBookingsByDateRange(DateTime from, DateTime to);
+        Booking GetById(int id);
+        void Update(Booking booking);
+    }
+}
