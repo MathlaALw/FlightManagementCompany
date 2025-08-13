0f8fcaeb8692ec8048f62766f1b2f9073e7f3042
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 16:16:14 +0400

    Add IRouteRepository interface and RouteRepository implementation

---
+++ Repository/IRouteRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IRouteRepository
+    {
+        void Add(Route route);
+        void Delete(Route route);
+        IEnumerable<Route> GetAll();
+        Route GetById(int id);
+        void Update(Route route);
+    }
+}
---
+++ Repository/RouteRepository.cs
@@ -0,0 +1,43 @@
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Repository
+{
+    public class RouteRepository : IRouteRepository
+    {
+
+        private readonly FlightDbContext _context;
+        public RouteRepository(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Route route)
+        {
+            _context.Routes.Add(route);
+            _context.SaveChanges();
+        }
+        public void Update(Route route)
+        {
+            _context.Routes.Update(route);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Route route)
+        {
+            _context.Routes.Remove(route);
+            _context.SaveChanges();
+        }
+
+        public Route GetById(int id) => _context.Routes.Find(id);
+
+        public IEnumerable<Route> GetAll() => _context.Routes.ToList();
+
+
+    }
+}
