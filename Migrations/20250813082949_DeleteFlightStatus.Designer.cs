19f93a42c8b5f8593217e409cbce8761c7e573a6
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-13 12:32:36 +0400

    Remove FlightStatus column from Flights table

--- Migrations/20250813082949_DeleteFlightStatus.Designer.cs
+++ Migrations/20250813082949_DeleteFlightStatus.Designer.cs
@@ -4,6 +4,7 @@ using FlightManagementCompany.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FlightManagementCompany.Migrations
 {
     [DbContext(typeof(FlightDbContext))]
-    partial class FlightDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250813082949_DeleteFlightStatus")]
+    partial class DeleteFlightStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250813082949_DeleteFlightStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightManagementCompany.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteFlightStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlightStatus",
+                table: "Flights");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FlightStatus",
+                table: "Flights",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
