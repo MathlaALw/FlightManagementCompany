23e27c8391731e659bccbaf72fd66e1c97a53cc5
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-14 10:50:38 +0400

    Make Program class public and initialize database
    Changed the `Program` class from `internal` to `public` for accessibility. Added `using` directives for `FlightManagementCompany.Data` and `FlightManagementCompany.Service`. Updated the `Main` method to ensure the database is created, instantiate `FlightService`, and populate it with sample data.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace FlightManagementCompany
+﻿using FlightManagementCompany.Data;
+using FlightManagementCompany.Service;
+
+namespace FlightManagementCompany
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // check if the database exists, if not create it
+            using FlightDbContext context = new FlightDbContext();
+            context.Database.EnsureCreated();
+            // Inject the context into the service
+            FlightService flightService = new FlightService(context);
+
+            // Create sample data
+            flightService.CreateSampleData();
+
         }
     }
 }
