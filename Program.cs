5469a90e012c58534fe8c1234f2dbfb12d08490f
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-14 16:10:01 +0400

    Add average load factor calculation feature
    Implement functionality to calculate the average load factor for flights within a specified date range. The code prompts the user for start and end dates, validates the input format, and utilizes the `GetAverageLoadFactor` method from the `FlightService` class.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,27 @@ namespace FlightManagementCompany
             // Create sample data
             // flightService.CreateSampleData();
 
+
+            // Average Load Factor
+            Console.WriteLine("Calculating Average Load Factor...");
+            Console.WriteLine("Enter the start date (yyyy-mm-dd):");
+            string startDateInput = Console.ReadLine();
+            DateTime startDate;
+            while (!DateTime.TryParse(startDateInput, out startDate))
+            {
+                Console.WriteLine("Invalid date format. Please enter the start date (yyyy-mm-dd):");
+                startDateInput = Console.ReadLine();
+            }
+            Console.WriteLine("Enter the end date (yyyy-mm-dd):");
+            string endDateInput = Console.ReadLine();
+            DateTime endDate;
+            while (!DateTime.TryParse(endDateInput, out endDate))
+            {
+                Console.WriteLine("Invalid date format. Please enter the end date (yyyy-mm-dd):");
+                endDateInput = Console.ReadLine();
+            }
+            var averageLoadFactor = flightService.GetAverageLoadFactor(startDate ,endDate);
+
         }
     }
 }
