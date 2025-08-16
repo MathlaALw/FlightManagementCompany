718cffaa915bfc8225c57cd0e56935684d8da662
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-16 18:57:30 +0400

    Add DTO classes for baggage and daily revenue

---
+++ DTO/BaggageOverweightDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.DTO
+{
+    public class BaggageOverweightDto
+    {
+
+        
+        public int TicketId { get; set; } // Ticket ID
+        public string PassengerName { get; set; } // Passenger Full Name
+        public double TotalBaggageWeight { get; set; } // Total baggage weight
+    }
+}
---
+++ DTO/DailyRevenueDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.DTO
+{
+    public class DailyRevenueDto
+    {
+        internal decimal CumulativeRevenue;
+
+        public DateTime Date { get; set; }
+        public decimal DailyRevenue { get; set; }
+
+    }
+}
